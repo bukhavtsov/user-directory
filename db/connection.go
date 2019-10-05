0ef0b510f918d8813df809fbe1fd4fd6e6b289cb
bukhavtsov <artyom.bukhavtsov@gmail.com>
2019-10-05 19:00:49 +0300

    db connection has been added

---
+++ db/connection.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func GetConnection(host, port, user, dbname, password, sslmode string) *gorm.DB {
+	args := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		host, port, user, dbname, password, sslmode)
+	connection, err := gorm.Open("postgres", args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return connection
+}
