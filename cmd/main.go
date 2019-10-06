baddb35005650721d06f2e6a6c71edc0b24b0d5d
bukhavtsov <artyom.bukhavtsov@gmail.com>
2019-10-06 21:12:30 +0300

    -handler has been added -env var for endpoint has been added

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/bukhavtsov/user-directory/db"
 	"github.com/bukhavtsov/user-directory/pkg/api"
 	"github.com/bukhavtsov/user-directory/pkg/data"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 const (
@@ -19,10 +21,28 @@ const (
 	sslmode  = "disable"
 )
 
+var (
+	serverEndpoint = os.Getenv("SERVER_ENDPOINT")
+)
+
+func init() {
+	if serverEndpoint == "" {
+		serverEndpoint = ":8080"
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	conn := db.GetConnection(host, port, user, dbname, password, sslmode)
 	defer conn.Close()
 	api.ServeUserResource(r, data.NewUser(conn))
-	log.Fatal(http.ListenAndServe(":8080", r))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	})
+	handler := cors.Default().Handler(r)
+
+	log.Println("serving server at ", serverEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
